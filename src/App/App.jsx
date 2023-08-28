afa04b003ccead9a9bb5d01b5afac0aac7333c04
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-28 18:29:43 +0430

    add comment to explain functions

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,16 +9,16 @@ import "shiftgrid"
 import './App.css';
 
 function App() {
+  // app state inital
   const [notes, setNotes] = useState([])
   const [inputValue, setInputValue] = useState("")
   const [localStorageKey, setLocalStorageKey] = useState("notes")
-  // get notes from local strage and setstate
+  // get notes from local strage and setstate on first time load
   useEffect(() => {
+    // it's return a array of object then set to state
     setNotes(getFromLocalStorage(localStorageKey))
-
-
   }, [localStorageKey])
-
+  // this function give a local storage key and return standard format array of item object
   function getFromLocalStorage(key) {
     // console.log("You try to get from local storage", key)
     if (key && typeof key === "string") {
@@ -35,6 +35,7 @@ function App() {
 
     }
   }
+  // this function set new object to local storage with key and object argumnet object foramt most be array to ittrable
   function setToLocalStorage(key, newObject) {
     // validate args value
     if (key && typeof key === "string" && typeof newObject === "object") {
@@ -45,19 +46,21 @@ function App() {
       console.log("Error to setitem in local storage")
     }
   }
+  // this function handle value change in main input get value and set to input state
   function handleInputValueChange(event) {
     let mainInputValue = event.target.value
     setInputValue(mainInputValue)
   }
-  function RandomColor() {
+  // this function give a colors array and return random item of array
+  function RandomColor(colors) {
     let randomInt = Math.floor(Math.random() * color.length)
-    return color[randomInt]
+    return colors[randomInt]
   }
-
+  // when press enter or + button call this function and submit new note in state
   function submitNewNote(value) {
     if (value && typeof value === "string") {
       // create new todo object
-      let bgColor = RandomColor()
+      let bgColor = RandomColor(color)
       let textColor = getContrastColor(bgColor)
       let newNote = {
         id: notes.length + 1,
@@ -65,7 +68,7 @@ function App() {
         backgroundColor: bgColor,
         color: textColor,
       }
-      // set new note on state
+      // set new note to state
       setNotes([...notes, newNote])
       // make input value empty
       setInputValue("")
@@ -73,6 +76,7 @@ function App() {
       setToLocalStorage(localStorageKey, [...notes, newNote])
     }
   }
+  // this function give a note id and remove from state and local storage - call when click on (x) button in note
   function removeNote(noteID) {
     let userConfirmResult = window.confirm("Do You Want to Remove This Note? \t" + noteID)
     if (userConfirmResult) {
@@ -87,7 +91,6 @@ function App() {
       console.log("user cancel note remove proccess! \t", noteID)
     }
   }
-
   return (
     <div className="app container">
       <div className="row">
@@ -113,7 +116,7 @@ function App() {
         <div className="row">
           {notes.map((note) => (
             <Note
-              className={"col g-10"}
+              className={"col"}
               key={note.id}
               id={note.id}
               value={note.value}

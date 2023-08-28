f5bce1c38ac18b8d3dbfffbbe658a360bbcf6375
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-08-28 12:23:34 +0430

    Handle Save notes on Local Storage

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Input from "../Components/Input/Input"
 import Note from "../Components/Note/Note"
 import "shiftgrid"
@@ -20,7 +20,41 @@ function App() {
     // { id: 10, value: "Clean and organize desk space", backgroundColor: "#3F51B5", color: "white" },
   ])
   const [inputValue, setInputValue] = useState("")
+  const [localStorageKey, setLocalStorageKey] = useState("notes")
+  // get notes from local strage and setstate
+  useEffect(() => {
+    setNotes(getFromLocalStorage(localStorageKey))
 
+
+  }, [localStorageKey])
+
+  function getFromLocalStorage(key) {
+    console.log("You try to get from local storage", key)
+    if (key && typeof key === "string") {
+      let dataFromLocalStorage = JSON.parse(localStorage.getItem(key))
+      // validate data from local storage  data !=== null
+      if (dataFromLocalStorage === null) {
+        console.log("Local storage is empty and return empty array")
+        return []
+      } else {
+        console.log(`Local storage have some data with ${key} key.`)
+        return dataFromLocalStorage
+
+      }
+
+    }
+  }
+  function setToLocalStorage(key, newObject) {
+    // validate args value
+    if (key && typeof key === "string" && typeof newObject === "object") {
+      console.log("You try to set new note to local storage", key, newObject)
+      // set new list to local storage object
+      localStorage.setItem(key, JSON.stringify(newObject))
+
+    } else {
+      console.log("Error to setitem in local storage")
+    }
+  }
   function handleInputValueChange(event) {
     let mainInputValue = event.target.value
     setInputValue(mainInputValue)
@@ -34,16 +68,19 @@ function App() {
     if (value && typeof value === "string") {
       // create new todo object
       let bgColor = RandomColor()
+      let txtColor = "white" // TODO : conditional contrast view
       let newNote = {
         id: notes.length + 1,
-        value: value,
+        value: value.trim(),
         backgroundColor: bgColor,
-        color: "white"
+        color: txtColor,
       }
       // set new note on state
       setNotes([...notes, newNote])
       // make input value empty
       setInputValue("")
+      // save new note on local storage
+      setToLocalStorage(localStorageKey, [...notes, newNote])
     }
   }
   function removeNote(noteID) {
@@ -52,9 +89,11 @@ function App() {
       let filteredNotes = notes.filter((note) => {
         return note.id !== noteID
       })
-
+      // update state
       setNotes(filteredNotes)
-    }else{
+      // update local storage
+      setToLocalStorage(localStorageKey, filteredNotes)
+    } else {
       console.log("user cancel note remove proccess! \t", noteID)
     }
   }
